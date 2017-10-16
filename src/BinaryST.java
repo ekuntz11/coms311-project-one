f7fddaf11f45b8d1032a5be7908f3a15af62f2c5
Merritt <mharvey@iastate.edu>
2017-10-16 13:48:55 -0500

    small amount of BST changes because I got distracted

--- src/BinaryST.java
+++ src/BinaryST.java
@@ -10,17 +10,14 @@
 
 public class BinaryST
 {
-	private int size;
-	private Node head;
-	//private int distinctsize; //not sure if we will need this... what do you think?
+	private int size, distinct_size, height;
+	private Node root;
 	//private int height; //also not sure about this one
 	private class Node {
-		public Node left;
-		public Node right;
-		public Node parent;
+		public Node left, right, parent;
 		public String value;
 		public int vals_in_left_tree; //need this for the rankOf method
-		//public int count; //this could be used in order to store the number of times the string appears... not sure which way is best for doing this
+		public int count; //tracks the number of times this string appears
 		
 		public Node(String val){
 			this(null, null, null, val);
@@ -37,22 +34,26 @@ public class BinaryST
 	
 	public BinaryST()
 	{
-		// implementation
+		size = 0;
+		root = null;
 	}
 	
 	public BinaryST(String[] s)
 	{
-		// implementation
+		for(int i=0; i<s.length; i++){
+			//the add method will deal with increasing size, distinct size, etc. 
+			this.add(s[i]);
+		}
 	}
 	
 	public int distinctSize()
 	{
-		// implementation
+		return distinct_size;
 	}
 	
 	public int size()
 	{
-		// implementation
+		return size;
 	}
 	
 	public int height()
@@ -67,12 +68,38 @@ public class BinaryST
 	
 	public boolean search(String s)
 	{
-		// implementation
+		if(root == null || s ==null) {
+            return false;
+        }
+       Node n =  findRec(s, root);
+       return n != null;
 	}
+	
+	//recursive helper method to help search and frequency methods
+	//returns null if the node was not found. if n = a node then it was found in the tree
+	private Node findRec(String key, Node n) {
+        if (n == null) {
+            return null;
+        } else {
+            if (n.value.equals(key)) {
+                return n;
+            } else if (n.value.compareTo(key) > 0) {
+                return findRec(key, n.left);
+            } else { // n.value < key
+                return findRec(key, n.right);
+            }
+        }
+
+    }
 	
 	public int frequency(String s)
 	{
-		// implementation
+		Node n = findRec(s, root);
+		if(n == null){
+			return 0;
+		} else {
+			return n.count;
+		}
 	}
 	
 	public boolean remove(String s)
